d1b61205618becdaf43e203b9066e0569ea1c933
shoyebzz <shoyeb.abir21@gmail.com>
2019-04-12 05:36:43 +0600

    dropdown resume 80% done

--- js/main.js
+++ js/main.js
@@ -34,5 +34,12 @@ jQuery(document).ready(function(){
 			}
 		});*/
 
+var getElement = $('.my-resume');
+
+$('#resume-caller').click(function(e){
+	e.preventDefault();
+	getElement.toggleClass("active");
+});
+
 		
 });
